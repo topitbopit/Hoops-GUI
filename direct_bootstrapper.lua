47da74b0618ee1882245f155caa54ad24c081f2f
topit <82128920+topitbopit@users.noreply.github.com>
2021-05-21 02:10:55 -0700

    direct link

---
+++ direct_bootstrapper.lua
@@ -0,0 +1,6 @@
+--[[Hoops GUI]]--
+--[[Made by topit]]--
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/topitbopit/Hoops-GUI/main/latest.lua', true))()
+
+--This link auto updates, so there's no need to update this file.
