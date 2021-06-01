ef57dd5fc1f926c5cf2c64798dc4d06b82333349
topit <82128920+topitbopit@users.noreply.github.com>
2021-05-31 21:45:45 -0700

    Update direct_bootstrapper.lua

--- direct_bootstrapper.lua
+++ direct_bootstrapper.lua
@@ -1,6 +1,4 @@
 --[[Jeff's Hoops GUI]]--
 --[[Made by topit]]--
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/topitbopit/Hoops-GUI/main/latest.lua', true))()
-
---This link auto updates, so there's no need to update this file.
+loadstring(game:HttpGet('https://raw.githubusercontent.com/topitbopit/Hoops-GUI/main/script.lua', true))()
